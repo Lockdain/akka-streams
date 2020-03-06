fa3e26b034b3eabd110c7b67ddc8fa3cbb9e609a
Alexander Sergeenko <asergeenko@neoflex.ru>
2020-03-06 17:15:14 +0300

    ++

--- src/main/scala/part1_recap/AkkaRecap.scala
+++ src/main/scala/part1_recap/AkkaRecap.scala
@@ -1,11 +1,22 @@
 package part1_recap
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorSystem, Props, Stash}
 
 object AkkaRecap extends App {
-  class SimpleActor extends Actor {
+  class SimpleActor extends Actor with Stash{
     override def receive: Receive = {
+      case "createChild" =>
+        val childActor = context.actorOf(Props)
+      case "stashThis" =>
+        stash()
+      case "change handler NOW" =>
+        unstashAll()
+      case "change" => context.become(anotherHandler)
       case message => println(s"I received: $message")
+    }
+
+    def anotherHandler: Receive = {
+      case message => println(s"In another receive handler: $message")
     }
   }
 
@@ -13,4 +24,18 @@ object AkkaRecap extends App {
   val system = ActorSystem("AkkaRecap")
   // actor instantiation
   val actor = system.actorOf(Props[SimpleActor], "simpleActor")
+
+  // #2: sending a message
+  actor ! "hello"
+
+  /*
+    - messages are sent asynchronously
+    - many actors (in the millions) can share a few dozen threads
+    - each message is processed atomically
+    - no need for locks
+   */
+
+  // changing actor behaviour + stashing
+
+  // actors can spawn other actors
 }
