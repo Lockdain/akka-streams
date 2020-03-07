7b0147899511d7c8f4465db87466c2d6ce21bce8
Alex Sergeenko <lockdain@gmail.com>
2020-03-07 16:19:56 +0300

    Akka recap added.

--- src/main/scala/part1_recap/AkkaRecap.scala
+++ src/main/scala/part1_recap/AkkaRecap.scala
@@ -1,12 +1,15 @@
 package part1_recap
 
-import akka.actor.{Actor, ActorSystem, Props, Stash}
+import akka.actor.SupervisorStrategy.Restart
+import akka.actor.{Actor, ActorLogging, ActorSystem, OneForOneStrategy, PoisonPill, Props, Stash, SupervisorStrategy}
+import akka.stream.Supervision.Stop
+import akka.util.Timeout
 
 object AkkaRecap extends App {
-  class SimpleActor extends Actor with Stash{
+  class SimpleActor extends Actor with Stash with ActorLogging {
     override def receive: Receive = {
       case "createChild" =>
-        val childActor = context.actorOf(Props)
+//        val childActor = context.actorOf(Props)
       case "stashThis" =>
         stash()
       case "change handler NOW" =>
@@ -17,6 +20,14 @@ object AkkaRecap extends App {
 
     def anotherHandler: Receive = {
       case message => println(s"In another receive handler: $message")
+    }
+
+    override def preStart(): Unit = {
+      log.info("I'm starting")
+    }
+
+    override def supervisorStrategy: SupervisorStrategy = OneForOneStrategy() {
+      case _: RuntimeException => Restart
     }
   }
 
@@ -38,4 +49,35 @@ object AkkaRecap extends App {
   // changing actor behaviour + stashing
 
   // actors can spawn other actors
+  // guardians: /system, /user, /root guardian
+
+  // actors have a defined lifecycle: started, stop, suspended, resume, restarted
+  // stopping actors - context.stop
+  actor ! PoisonPill
+
+  // logging
+
+  // supervision
+
+  // configure Akka infrastructure: dispatchers, routers, mailboxes
+
+  // schedulers
+  import scala.concurrent.duration._
+  import system.dispatcher
+
+  system.scheduler.scheduleOnce(2 seconds) {
+    actor ! "delayed happy birthday!"
+  }
+
+  // Akka patterns
+  // FSM + ask pattern
+  import akka.pattern.ask
+  implicit val timeout = Timeout(3 seconds)
+
+  val future = actor ? "Question"
+
+  // the pipe pattern
+  import akka.pattern.pipe
+  val anotherActor = system.actorOf(Props[SimpleActor], "anotherSimpleActor")
+  future.mapTo[String].pipeTo(anotherActor)
 }
