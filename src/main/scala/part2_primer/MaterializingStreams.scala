08908d79d22f5f4b24277f8e052e69ba566da8a8
Lockdain <lockdain@gmail.com>
2020-03-08 11:06:33 +0300

    Materialized values from graphs were added.

--- src/main/scala/part2_primer/MaterializingStreams.scala
+++ src/main/scala/part2_primer/MaterializingStreams.scala
@@ -1,16 +1,16 @@
 package part2_primer
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, RunnableGraph, Sink, Source}
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object MaterializingStreams extends App {
-  implicit val system = ActorSystem("MaterializingStreams")
-  implicit val materializer = ActorMaterializer()
+  implicit val system: ActorSystem = ActorSystem("MaterializingStreams")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   val simpleGraph = Source(1 to 10).to(Sink.foreach(println))
   private val simpleMaterializedValue: NotUsed = simpleGraph.run()
@@ -26,5 +26,15 @@ object MaterializingStreams extends App {
   }
 
   // Each component may return ONLY ONE materialized value!
+  // By default the most left materialized value is kept
+
+  // How to choose a materialized value
+  val simpleSource = Source(1 to 10)
+  val simpleFlow = Flow[Int].map(x => x + 1)
+  val simpleSink = Sink.foreach[Int](println)
+  // viaMat is able to compose materialized values from  previous components to a new one
+//  simpleSource.viaMat(simpleFlow) ((sourceMat, flowMat) => flowMat) // the long way
+  private val value: RunnableGraph[Future[Done]] = simpleSource.viaMat(simpleFlow)(Keep.right).toMat(simpleSink)(Keep.right)
+  value // syntactic sugar with additional sink
 
 }
